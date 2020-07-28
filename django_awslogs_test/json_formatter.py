2355ae320472cdc3a9dece2c69db6c784b30a7ec
missterr <maxim.kopytov@3d4medical.com>
2020-07-28 18:42:10 +0300

    A trial to change awslogs format

--- django_awslogs_test/json_formatter.py
+++ django_awslogs_test/json_formatter.py
@@ -19,7 +19,7 @@ class CustomisedJSONFormatter(JSONFormatter):
             extra.pop(field, None)
 
         if 'asctime' not in extra:
-            extra['asctime'] = timezone.now().isoformat()
+            extra['asctime'] = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
 
         if record.exc_info:
             extra['exc_info'] = self.formatException(record.exc_info)
